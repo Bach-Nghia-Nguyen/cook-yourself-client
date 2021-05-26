08a0b1b5447c9bcb267a6167f24a1b873ee8f81c
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2021-05-26 16:35:22 +0700

    comment out the unnecessaries

--- src/containers/layouts/PublicLayout.js
+++ src/containers/layouts/PublicLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
 import { Switch, Route } from "react-router-dom";
 
 import AlertMessage from "../../components/AlertMessage";
